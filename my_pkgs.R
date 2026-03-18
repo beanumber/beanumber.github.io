c940beaab6b7b58144ef2984cb9187844fb70f3e
Ben Baumer <ben.baumer@gmail.com>
2026-03-18 09:44:26 -0400

    separated cran and gh packages

--- my_pkgs.R
+++
@@ -1,7 +0,0 @@
-unlink("pkgs.bib")
-pkgs <- c("openWAR", "teamcolors", "infer", 
-          "airlines", "fec", "etl", "imdb", 
-          "fec16", "fertile",
-          "mdsr", "macleish", "tidychangepoint")
-
-knitr::write_bib(pkgs, file = "pkgs.bib")
